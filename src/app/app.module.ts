7cf6e2d58604c822fc395d42d29fe8348a2ee8d8
clintSanch <clintonodira@gmail.com>
2023-08-14 22:58:54 -0400

    @nguniversal embedment in to the system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

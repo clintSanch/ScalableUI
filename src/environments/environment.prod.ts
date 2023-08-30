3827965ebbce5e043b55b681f0466a4a08487cf1
clintSanch <clintonodira@gmail.com>
2023-08-30 13:50:32 -0400

    update on env.production file on the url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : true,
     environmentName: "production",
-    apiUrl: "https://vercel.org/clintSanch/scalable-ui"
+    apiUrl: "https://scalable-ui.vercel.app/"
 }

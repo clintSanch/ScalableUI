3b7f101d99ab113e21eba632666a7bed895d6196
clintSanch <clintonodira@gmail.com>
2023-09-16 16:42:12 -0400

    files/folder permission

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HeaderDirective } from './header.directive';
+import { RegisterComponent } from 'src/core/components/register/register.component';
+import { LoginComponent } from 'src/core/components/login/login.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   constructor(){}
+
+  @ViewChild(HeaderDirective, { static: true}) appHeader!: HeaderDirective
 
   async ngOnInit(){}
 
